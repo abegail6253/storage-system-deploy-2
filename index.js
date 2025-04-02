d9e44766c8a460acbd38857135f3ae9d0b68933c
aby <27abyombrosa@gmail.com>
2025-04-02 09:03:01 +0800

    update

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 pool.query('SELECT 1', (err, results) => {
